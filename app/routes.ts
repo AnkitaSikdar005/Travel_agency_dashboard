ea9e3f30a157c28a5e575a8644b28c14ebb59534
Ankita Sikdar <ank.sikdar2003@gmail.com>
2025-04-29 14:44:25 +0530

    Added files

---
+++ app/routes.ts
@@ -0,0 +1,11 @@
+import { type RouteConfig, route , layout } from "@react-router/dev/routes";
+
+export default [
+    route('sign-in', 'routes/root/sign-in.tsx'),
+    layout("routes/admin/admin-layout.tsx",[
+        route('dashboard', 'routes/admin/dashboard.tsx'),
+        route('all-users', 'routes/admin/all-users.tsx'), 
+    ]),
+   
+
+] satisfies RouteConfig;
